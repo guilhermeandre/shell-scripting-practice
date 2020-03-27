fba4bade6afa6063ef876d456cec0c7e3e058abc
Guilherme Andre dos Santos <guilhermeandredossantos@Guilhermes-iMac.local>
2020-03-26 19:47:02 -0700

    Operation selection (read) added.

--- calc.sh
+++ calc.sh
@@ -4,7 +4,7 @@
 echo "Hello, welcome to the most advanced calculator program based on Shell you will ever see!" 
 
 # Then, display a menu that outlines the possible operations:
-echo "Please make a selection:"
+echo "The possible operations are:"
   # Add
 echo "Add"
   # Subtract
@@ -13,6 +13,12 @@ echo "Substract"
 echo "Exit"
 
 # Then, capture the user selection.
+
+echo "Please make a selection:"
+read selection
+#Display the selection
+echo "The operations selected was: $selection"
+
 # If the selection matches a supported operation, execute the operation.
 # If the selection does not match a support operation, display an error message.
 # When the operation is complete, redisplay the menu.
