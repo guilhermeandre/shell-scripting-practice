ccbfe5926e72fa2233e38f29eb556c474a243acb
Guilherme Andre dos Santos <guilhermeandredossantos@Guilhermes-iMac.local>
2020-03-26 20:28:09 -0700

    While structure add to keep prompting options. Exit typing variable exit added.

--- calc.sh
+++ calc.sh
@@ -1,8 +1,11 @@
 #! /usr/bin/env bash
 
 # When the program is first loaded, display a greeting to the user.
-echo "Hello, welcome to the most advanced calculator program based on Shell you will ever see!" 
+echo "Hello, welcome to the most advanced calculator program based on Shell you will ever see!"
 
+# Keep prompting the Operations options until the user select to exit the calculator.
+
+while true; do
 # Then, display a menu that outlines the possible operations:
 echo "The possible operations are:"
   # Add
@@ -43,15 +46,16 @@ read second_number
    echo "The result is:"
 expr $first_number - $second_number
    ;;
-  "Exit")
+  "Exit" | "exit")
   echo "Exiting the calculator..."
+  exit
   ;;
 # If the selection does not match a support operation, display an error message.
   *)
   echo "Neither available operations were selected"
   ;;
 esac
+done
 
 
-# When the operation is complete, redisplay the menu.
 
