8a7c7993b9ba06564a565bd82954d6696a663405
Guilherme Andre dos Santos <guilhermeandredossantos@Guilhermes-iMac.local>
2020-03-26 19:41:23 -0700

    Instructions added. Set of operations added.

--- calc.sh
+++ calc.sh
@@ -1,13 +1,19 @@
 #! /usr/bin/env bash
 
 # When the program is first loaded, display a greeting to the user.
+echo "Hello, welcome to the most advanced calculator program based on Shell you will ever see!" 
+
 # Then, display a menu that outlines the possible operations:
+echo "Please make a selection:"
   # Add
+echo "Add"
   # Subtract
+echo "Substract"
   # Exit
+echo "Exit"
+
 # Then, capture the user selection.
 # If the selection matches a supported operation, execute the operation.
 # If the selection does not match a support operation, display an error message.
 # When the operation is complete, redisplay the menu.
 
-echo "Hello, welcome to the most advanced calculator program based on Shell you will ever see!"
